e3aa32fb9879cd10becd3d12f0ec38a97266290c
Borisaye <Boris_aroche@hotmail.com>
2023-03-21 12:36:44 -0400

    .

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let custDistance = "";
+
+function scuberGreetingForFeet(custDistance){
+  if (custDistance <= 400) {
+    return "This one is on me!"
+  }
+  else if (custDistance <= 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (custDistance < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+
+  }
+
+  }
+
+function ternaryCheckCity(city){
+  return (city === "NYC") ? "Ok, sounds good." : "No go.";
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+
+
+  }
+  
 }
